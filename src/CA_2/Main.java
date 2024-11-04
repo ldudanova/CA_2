5fa261a86ca05377027918b808af122274f3c46e
ldudanova <l.dudanova92@gmail.com>
2024-11-04 16:56:58 +0000

    Change structure. Add an empty report file

---
+++ src/CA_2/Main.java
@@ -0,0 +1,21 @@
+package CA_2;
+
+//Imports
+
+/**
+ * Main class
+ */
+public class Main {
+    /**
+     * Entry point
+     *
+     * @param args startup arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Start of program");
+
+
+        System.out.println("End of program");
+    }
+}
