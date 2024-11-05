abcc34105480e54a6d93621d138c99bdf02ee999
ldudanova <l.dudanova92@gmail.com>
2024-11-05 12:57:44 +0000

    Change InputUtilities: add selectFromList methods

--- src/CA_2/Utils/InputUtilities.java
+++ src/CA_2/Utils/InputUtilities.java
@@ -1,7 +1,6 @@
 package CA_2.Utils;
 
 //import java utils
-
 import java.util.Scanner;
 import java.util.function.Function;
 
@@ -168,5 +167,94 @@ public class InputUtilities {
         return num;
     }
 
+    /**
+     * Display a list of options for the user to select from,
+     * then prompt them to enter a valid option.
+     * If the selection is invalid, display an error and re-prompt.
+     *
+     * This method is used to present a simple list of String options to the user.
+     * It directly displays the options in the String array,
+     * which are passed in as a variable-length argument list.
+     * The method returns the index of the chosen item.
+     *
+     * @param prompt  The prompt message to display.
+     * @param options The list of options available to choose from.
+     * @return The index of the selected option.
+     */
+    public static int selectFromList(String prompt, String... options) {
+        // If the list of options is empty, display an error and return 0
+        if (options.length == 0) {
+            System.err.println("List of values is empty!");
+            return 0;
+        }
+
+        // Display the prompt message
+        System.out.println(prompt);
+
+        int index = 1;
+        // Display each option with a numbered index
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ") " + options[i]);
+        }
+
+        int choice = askUserForInt("Choose an option (1-" + options.length + "): ", 1, options.length);
+        return choice - 1; // Return the index of the chosen option
+    }
+
+    /**
+     * Display a list of options derived from an Enum class for the user to select from.
+     * If the selection is invalid, display an error and re-prompt.
+     *
+     * This method is specifically designed to work with enums.
+     * It converts each enum constant into a string for display and lets the user select one.
+     * The method returns the chosen enum constant itself rather than an index,
+     * making it more type-safe and convenient when working with enums.
+     *
+     * @param prompt   The prompt message to display.
+     * @param enumData The enum class providing options for selection.
+     * @return The selected Enum value.
+     */
+    public static <E extends Enum<E>> E selectFromList(String prompt, Class<E> enumData) {
+        // Retrieve all constants from the Enum class
+        E[] enumConstants = enumData.getEnumConstants();
+
+        System.out.println(prompt);
+
+        for (int i = 0; i < enumConstants.length; i++) {
+            System.out.println((i + 1) + ") " + enumConstants[i]);
+        }
+
+        int choice = askUserForInt("Choose an option (1-" + enumConstants.length + "): ", 1, enumConstants.length);
+        return enumConstants[choice - 1];
+    }
+
+    /**
+     * Display a list of options derived from an array of objects. Prompt the user to select an item.
+     * If the selection is invalid, display an error and re-prompt.
+     *
+     * This method is used to have a more complex object array
+     * and to display specific attributes of each object (not just String values).
+     * The Function<E, String> converter allows to customize the string representation for each item in the array.
+     * It returns the selected item from the array, providing flexibility for objects of any type,
+     * not just strings or enums.
+     *
+     * @param prompt    The prompt message to display.
+     * @param array     The array of objects to select from.
+     * @param <E>       The type of objects in the array.
+     * @param converter Function to convert each object in the array to a displayable string.
+     * @return The selected item from the array.
+     */
+    public static <E> E selectFromList(String prompt, E[] array, Function<E, String> converter) {
+        // Array to hold the string representations
+        String[] values = new String[array.length];
+
+        // Convert each object in the array to a string using the provided converter function
+        for (int i = 0; i < array.length; i++) {
+            values[i] = (converter.apply(array[i]));
+        }
+
+        // Prompt the user to select an item and return the selected item
+        return array[selectFromList(prompt, values)];
+    }
 }
 
