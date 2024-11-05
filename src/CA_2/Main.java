dbe475cef5c46d17128cfd30fb887d4d28e1f198
ldudanova <l.dudanova92@gmail.com>
2024-11-05 21:44:13 +0000

    Change Main: add reading file and return parsed people and printing first 20 people from the file and showing the menu

--- src/CA_2/Main.java
+++ src/CA_2/Main.java
@@ -2,6 +2,11 @@ package CA_2;
 
 //Imports
 
+import CA_2.UI.Menu;
+import CA_2.UI.Printer;
+import CA_2.Utils.Reader;
+import CA_2.Utils.Store;
+
 /**
  * Main class
  */
@@ -15,6 +20,14 @@ public class Main {
 
         System.out.println("Start of program");
 
+        // Read file and return parsed people
+        Store.people = Reader.readFile();
+
+        // Print first 20 people from the file
+        Printer.printPeople(Store.people, 20);
+
+        // Start menu loop
+        Menu.showMenu();
 
         System.out.println("End of program");
     }
